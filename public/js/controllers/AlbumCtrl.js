749e4713e0c7fe6b5568363399ebf4a365b8d081
pszklanko <pszklanko@gmail.com>
2016-12-12 22:35:10 +0100

    modal part 2

--- public/js/controllers/AlbumCtrl.js
+++ public/js/controllers/AlbumCtrl.js
@@ -46,26 +46,5 @@ angular.module('AlbumCtrl', []).controller('AlbumController', function($scope, $
       });
   };
 
-  $scope.animationsEnabled = true;
-  $scope.open = function (size, parentSelector) {
-    var parentElem = parentSelector ?
-      angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
-    var modalInstance = $uibModal.open({
-      animation: $scope.animationsEnabled,
-      ariaLabelledBy: 'modal-title',
-      ariaDescribedBy: 'modal-body',
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      controllerAs: '$ctrl',
-      size: size,
-      appendTo: parentElem,
-      resolve: {
-        items: function () {
-          return $scope.items;
-        }
-      }
-    });
-  };
-
   $scope.getAlbums();
 });
